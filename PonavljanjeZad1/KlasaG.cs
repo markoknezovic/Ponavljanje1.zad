330d69cb1db7d47aa7a613eb4e59fdb03c65c22e
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-13 20:33:00 +0200

    Add files via upload

---
+++ PonavljanjeZad1/KlasaG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljanjeZad1
+{
+    class KlasaG
+    {
+        public void plus10(int a)
+        {
+
+            a = a * 10;
+            Console.WriteLine(a);
+        }
+        public void f50(int b)
+        {
+
+            b = 50;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ PonavljanjeZad1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljanjeZad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("broj: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            KlasaG plus = new KlasaG();
+            KlasaG mnozenje = new KlasaG();
+
+            Console.Write("Zbrajanje: ");
+            plus.f50(a);
+            Console.Write("Mnozenje: ");
+            mnozenje.plus10(a);
+            Console.ReadLine();
+        }
+    }
+}
